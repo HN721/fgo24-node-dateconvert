62c57a639f8766baf31b276bccefbd28c421a7f1
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-23 11:22:27 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import moment from "moment";
+import readline from "readline";
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("Masukan Tanggal = ", (date) => {
+  if (date.length !== 10) {
+    console.log("Format Tanggal Salah");
+  } else {
+    const input = moment(date, "DD-MM-YYYY").format("DD/MM/YYYY");
+    if (input === "Invalid date") {
+      console.log("Format Tanggal Salah");
+    } else {
+      console.log(`${input}`);
+    }
+  }
+
+  //   rl.close();
+});
