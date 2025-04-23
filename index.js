f81c59a7ab4e2cc78a68bfaf2dd8a43ed69d14ce
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-23 15:20:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,71 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 rl.question("Masukan Tanggal = ", (date) => {
-  if (date.length !== 10 || date.includes("/")) {
-    console.log("Format Tanggal Salah");
-  } else {
-    const input = moment(date, "DD-MM-YYYY").format("DD/MM/YYYY");
-    if (input === "Invalid date") {
-      console.log("Format Tanggal Salah");
+  try {
+    if (date.length !== 10 || date.includes("/")) {
+      const y = new Error("Harus 10 Angka");
+      throw y;
     } else {
-      console.log(`${input}`);
+      const input = moment(date, "DD-MM-YYYY").format("DD/MM/YYYY");
+      if (input === "Invalid date") {
+        throw new Error("Invalid date");
+      } else {
+        console.log(`${input}`);
+      }
+    }
+
+    rl.close();
+  } catch (err) {
+    if (err.message === "Harus 10 Angka") {
+      console.log("Harap Masukan Tanggal Sesuai Format");
+    }
+    if (err.message === "Invalid Date") {
+      console.log("Format Tanggal Salah");
     }
   }
-
   rl.close();
 });
 // Tanpa Package
 
 rl.question("Masukan Tanggal : ", (input) => {
-  const data = input.split("-").join("/");
-  if (data.length !== 10 || /[a-zA-Z]/.test(data)) {
-    console.log("Format Tanggal Salah");
-  }else{
-  console.log(data)
+  try {
+    const data = input.split("-").join("/");
+    if (data.length === 10) {
+      throw new Error("Harus 10 Angka");
+    }
+    if (/[a-zA-Z]/.test(data)) {
+      throw new Error("Harap Masukan Huruf");
+    }
+    rl.close();
+  } catch (err) {
+    if (err.message === "Harus 10 Angka") {
+      console.log("Harap Masukan Tanggal Sesuai Format");
+    }
+    if (err.message === "Harap Masukan Huruf") {
+      console.log("Format Tanggal Salah");
+    }
+    rl.close();
   }
-  rl.close();
 });
+// function sebuahProgram(kondisi) {
+//   if (typeof kondisi !== "boolean") {
+//     const y = new Error("tipe_data");
+//     throw y;
+//   }
+//   if (!kondisi) {
+//     const x = new Error("false");
+//     throw x;
+//   }
+//   return `Program Berjalan`;
+// }
+// try {
+//   const result = sebuahProgram(false);
+//   console.log(result);
+// } catch (err) {
+//   if (err.message === "tipe_data") {
+//     console.log("Tipe Data tidak sesuai");
+//   }
+//   if (err.message == "false") {
+//     console.log("Parameter tidak boleh diisikan flase");
+//   }
+// }
